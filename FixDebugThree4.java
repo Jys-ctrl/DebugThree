a40b71409e7caf5d06eeb5cd338783c4c0cfcf2f
Joyce Balante <43256976+Juysi@users.noreply.github.com>
2021-03-09 16:06:42 +0800

    Add files via upload

---
+++ FixDebugThree4.java
@@ -0,0 +1,19 @@
+//This program calculates tuition bills
+//as credits times rate per credit hour
+public class FixDebugThree4
+{
+public static void main(String args[])
+{
+   int myCredits = 13;
+   int yourCredits = 17;
+   double rate = 75.84;
+   System.out.println("My tuition:");
+   TuitionBill(myCredits, rate);
+   System.out.println("Your tuition:");
+   TuitionBill(yourCredits, myCredits);
+}
+public static void TuitionBill(int c, double r)
+{
+   System.out.println("Total due " + (r / c));
+}
+}
